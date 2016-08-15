b0f70e0c06341373dd07b8ebee667b223a162a0e
benhallouk <benhallouk@gmail.com>
2016-08-15 22:40:38 +0100

    feat(backlog-api): add the backlog list api
    this will be used from angular to get all the backlog items

--- backend/backlog/api.py
+++ backend/backlog/api.py
@@ -1,12 +1,14 @@
 from flask_login import login_required, login_user, logout_user, current_user
 from flask import jsonify
 
+import jsonpickle
+
 from . import backlog
 from .. import db
 from ..models.BacklogItem import BacklogItem
 
 @backlog.route("/backlog", methods=["GET"])
-#@login_required
+@login_required
 def backlog():
-    all = BacklogItem.query.all();
-    return jsonify(all);
+    all = BacklogItem.query.all()
+    return jsonpickle.encode(all) , 200, {'Content-Type': 'application/json charset=utf-8'}
